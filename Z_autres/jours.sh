f52d35f2fbeb05059e459927c00a0b6fa1123852
Stéphane MEYER <tigerlost@me.com>
2022-03-20 09:06:04 +0100

    cleanup

--- Z_autres/jours.sh
+++ Z_autres/jours.sh
@@ -1,21 +1,25 @@
 #! /usr/bin/env bash
 
-MOIS=( "Janvier" "Février" "Mars" "Avril" "Mai" "Juin" "Juillet" "Août" "Septembre" "Octobre" "Novembre" "Décembre" )
+MOIS=( "Jan" "Fév" "Mar" "Avr" "Mai" "Jun" "Jul" "Aoû" "Sep" "Oct" "Nov" "Déc" )
 
 read -rp "Mois  : " mois
 
 # On vérifie si un nombre a bien été saisi...
 [[ $mois =~ [0-9]+ ]] || { echo "Erreur !"; exit 1; }
+
 # ... et si le mois entré est bien compris entre 1 et 12.
 (( mois >= 1 && mois <= 12 )) || { echo "Erreur !"; exit 1; }
 
 # On demande l'année si le mois saisi est égal à 2.
 (( mois == 2 )) && {
   read -rp "Année : " annee
+
   # On vérifie la validité de la saisie.
   [[ $annee =~ [0-9]+ ]] || { echo "Erreur !"; exit 1; }
+
   # Si l'année est bissextile, on assigne 29 à la variable jours, sinon 28.
   ((jours=( (annee % 4 == 0) && !(annee % 100== 0) || (annee % 400 == 0) ) == 1 ? 29 : 28))
+
   # On affiche le résultat.
   echo "${MOIS[$((mois-1))]} : $((jours)) jours."
   exit 0
@@ -24,21 +28,32 @@ read -rp "Mois  : " mois
 # Soit 1 les mois de 31 jours 
 # et   0 les mois de 30 jours
 # on obtient :
-# 101010110101 (Ce qui en décimal donne le nombre 2741 et en hexadécimal, AB5.)
-# On déplace le nombre vers la droite de 11, 10, 9 rangs... jusqu'à 0, on obtient :
-# (1)----------- 11 Janvier
-# 1(0)---------- 10 Février
-# 10(1)--------- 09 Mars
-# 101(0)-------- 08 Avril
-# 1010(1)------- 07 Mai
-# 10101(0)------ 06 Juin
-# 101010(1)----- 05 Juillet
-# 1010101(1)---- 04 Août
-# 10101011(0)--- 03 Septembre
-# 101010110(1)-- 02 Octobre
-# 1010101101(0)- 01 Novembre
-# 10101011010(1) 00 Décembre
-# Et on extrait le bit le plus à droite.
+#
+# Jan Fév Mar Avr Mai Jun Jul Aoû Sep Oct Nov Déc
+# 11  10  09  08  07  06  05  04  03  02  01  00
+# 1   0   1   0   1   0   1   1   0   1   0   1
+#
+# 101010110101, ce qui en décimal donne le nombre 2741 et en hexadécimal, 0xAB5.
+#
+# En procédant à un déplacement du nombre vers la droite (right shift) de 11, 10,
+# 9 rangs... jusqu'à 0, on obtient :
+#
+# (OxAB5 >> 11) : 1-----------
+# (OxAB5 >> 10) : 10----------
+# (OxAB5 >> 09) : 101---------
+# (OxAB5 >> 08) : 1010--------
+# (OxAB5 >> 07) : 10101-------
+# (OxAB5 >> 06) : 101010------
+# (OxAB5 >> 05) : 1010101-----
+# (OxAB5 >> 04) : 10101011----
+# (OxAB5 >> 03) : 101010110---
+# (OxAB5 >> 02) : 1010101101--
+# (OxAB5 >> 01) : 10101011010-
+# (OxAB5 >> 00) : 101010110101
+#
+# Et on isole le bit le plus à droite.
+
 ((jours=((0xAB5 >> 12 - mois) & 1) == 1 ? 31 : 30))
+
 # On affiche le résultat.
 echo "${MOIS[$((mois-1))]} : $((jours)) jours."
