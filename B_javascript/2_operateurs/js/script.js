088b2d72e199440f860a35eee570d3a7c99149b6
Stéphane MEYER <tigerlost@me.com>
2022-03-21 09:53:42 +0100

    changed js exercise 2

--- B_javascript/2_operateurs/js/script.js
+++ B_javascript/2_operateurs/js/script.js
@@ -1,11 +1,11 @@
 var a = "100";
 var b = 100;
-var c = 1.0
-var d = true
+var c = 1.0;
+var d = true;
 
 alert("Ceci est une chaîne de caractères : " + a);
 b--;
-c += a;
+c += parseInt(a);
 d = !d;
 
 console.log("b=" + b);
