22913fc3ffd80d4f6e563e25000fbc981595edd7
Stéphane MEYER <tigerlost@me.com>
2022-03-22 12:03:41 +0100

    check if entered number is not NaN

--- B_javascript/4_boucles/4_multiples/js/script.js
+++ B_javascript/4_boucles/4_multiples/js/script.js
@@ -1,7 +1,7 @@
 var n = parseInt(prompt("N"));
 var x = parseInt(prompt("X"));
 
-if (n > 0 && x > 0) {
+if (n > 0 && x > 0 && !Number.isNaN(n)) {
   for (var i = 1; i <= n; i++) {
     console.log(i + " x " + x + " = " + (i*x));
   }
