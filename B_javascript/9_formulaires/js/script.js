6f5022f63ef54e72bd74deaa8e07a510d01ff369
Stéphane MEYER <tigerlost@me.com>
2022-03-23 09:37:29 +0100

    added checkForm() function

--- B_javascript/9_formulaires/js/script.js
+++ B_javascript/9_formulaires/js/script.js
@@ -6,3 +6,50 @@ function updateText() {
     document.getElementById("techenv-text").value = text;
   }
 }
+
+function checkForm() {
+  var text = document.getElementById("company").value;
+  var regex = new RegExp("[A-Za-z0-9_-]+");
+  var result = regex.test(text)
+
+  if (!result) {
+    alert("Entrez le nom de la société svp !");
+    return
+  }
+
+  text = document.getElementById("person").value;
+  result = regex.test(text);
+
+  if (!result) {
+    alert("Entrez le nom de la personne à contacter svp !");
+    return
+  }
+
+  text = document.getElementById("city").value;
+  result = regex.test(text);
+
+  if (!result) {
+    alert("Entrez la ville svp !");
+    return
+  }
+
+  text = document.getElementById("zipcode").value;
+  regex = new RegExp("[0-9]{5}");
+  result = regex.test(text);
+
+  if (!result) {
+    alert("Entrez le code postal sur 5 chiffres svp !");
+    return
+  }
+
+  text = document.getElementById("email").value;
+  regex = new RegExp(".+@.+");
+  result = regex.test(text);
+
+  if (!result) {
+    alert("Entrez une adresse e-mail valide svp !");
+    return
+  }
+
+  document.contact.submit();
+}
