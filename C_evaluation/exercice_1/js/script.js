1b258a17fbb512971acdb7ed7b0d926948078e9f
Stéphane MEYER <tigerlost@me.com>
2022-03-23 20:50:54 +0100

    check for NaN

--- C_evaluation/exercice_1/js/script.js
+++ C_evaluation/exercice_1/js/script.js
@@ -3,13 +3,20 @@ var age = 0;
 
 while (age < 100) {
   age = parseInt(prompt('Entrer âge :'));
-
-  if (age <= 0) continue;
+  // console.log(age);
+  // console.log('isNaN? ' + (Number.isNaN(age)));
+  // console.log('NaN<100? ' + (age < 100))
+  if (age <= 0 || Number.isNaN(age)) {
+    age = 0; // Pour une raison que j'ignore si age == NaN, la boucle se termine...
+    continue;
+  }
   if (age < 20) ages.J.push(age);
   else if (age > 40 && age < 100) ages.V.push(age);
   else if (age >= 20 && age <= 40) ages.M.push(age);
   else if (age >= 100) ages.V.push(age);
 }
+
+// console.log('loop end: ' + age);
 
 console.log('Nombre de jeunes : ' + ages.J.length);
 console.log('Nombre de moyens : ' + ages.M.length);
