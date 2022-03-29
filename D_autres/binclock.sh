411787f738ff420adf97578043b7b4b85d59a1ee
Stéphane MEYER <tigerlost@me.com>
2022-03-29 21:18:03 +0200

    improved bash script

--- D_autres/binclock.sh
+++ D_autres/binclock.sh
@@ -1,45 +1,82 @@
 #! /usr/bin/env bash
 
 B=("0000" "0001" "0010" "0011" "0100" "0101" "0110" "0111" "1000" "1001")
+# G="84218421"
+
+b="\e[1m"      # bold
+d="\e[2m"      # dim
+c="\e[38;5;4m" # color
+r="\e[m"       # reset
 
 savecursor()    { echo -ne "\e[s" ;}
 restorecursor() { echo -ne "\e[u" ;}
 hidecursor()    { echo -ne "\e[?25l" ;}
 showcursor()    { echo -ne "\e[?25h" ;}
 
-hidecursor; stty -echo; clear
+exec {pause_fd}<> <(:)
+pause() ( read -rt "$1" -u $pause_fd )
 
-trap 'showcursor; stty echo; exit' INT QUIT
+_sync() {
+  local n=1
+  local N
+  while (( n !=0 )); do
+    N="${EPOCHREALTIME#*,}"
+    n=${N:0:1}
+    pause 0.0625
+  done
+}
+
+hidecursor; stty -echo -icanon time 0 min 0; clear
+
+trap 'echo -en "\e[m"; showcursor; stty echo; exit' INT QUIT
 
 savecursor
 
-
 while :; do
-  IFS= read -rsn 1 -t 0.25 key
+  # shellcheck disable=SC2162
+  IFS= read key
   case $key in
     q | Q) clear; break ;;
     r | R) clear; savecursor ;;
   esac
+
+  _sync
 
   TIME="$(date '+%H %M %S')"
 
   # shellcheck disable=SC2162
   IFS=' ' read H M S <<< "$TIME"
 
-  D=("${H:0:1}" "${H:1:1}" "${M:0:1}" "${M:1:1}" "${S:0:1}" "${S:1:1}")
 
-  i=0
-  for d in "${D[@]}"; do
-    echo -ne "\e[38;5;1m\e[1m"
-    echo -en "${B["$d"]}"
-    ((i++))
-    (( i % 2 == 0 )) && echo
-    # (( i % 2 == 0 )) || echo -n "."
+  # echo -en "\e[38;5;1m"
+  # echo "$G"
+
+  h="${B[${H:0:1}]}${B[${H:1:1}]}"
+  m="${B[${M:0:1}]}${B[${M:1:1}]}"
+  s="${B[${S:0:1}]}${B[${S:1:1}]}"
+
+  for ((i=0;i<${#h};i++)); do
+    (( ${h:i:1} == 1 )) && echo -en "${c}${b}1${r}"
+    (( ${h:i:1} == 0 )) && echo -en "${c}${d}0${r}"
   done
+  echo
+
+  for ((i=0;i<${#m};i++)); do
+    (( ${m:i:1} == 1 )) && echo -en "${c}${b}1${r}"
+    (( ${m:i:1} == 0 )) && echo -en "${c}${d}0${r}"
+  done
+  echo
+
+  for ((i=0;i<${#s};i++)); do
+    (( ${s:i:1} == 1 )) && echo -en "${c}${b}1${r}"
+    (( ${s:i:1} == 0 )) && echo -en "${c}${d}0${r}"
+  done
+  echo
 
   restorecursor
 
-  sleep 0.25
+  pause 0.5
 done
 
+echo -en "\e[m"
 stty echo; showcursor
